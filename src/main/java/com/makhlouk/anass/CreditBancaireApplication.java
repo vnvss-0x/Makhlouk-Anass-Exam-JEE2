b8d6fb580b307057b801eea99e669e1bc077c931
vnvss-0x <anassmakhlo@gmail.com>
2026-06-08 10:05:55 +0100

    Initial commit

---
+++ src/main/java/com/makhlouk/anass/CreditBancaireApplication.java
@@ -0,0 +1,12 @@
+package com.makhlouk.anass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditBancaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditBancaireApplication.class, args);
+    }
+}
---
+++ src/test/java/com/makhlouk/makhloukanassexamjee2/MakhloukAnassExamJee2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makhlouk.makhloukanassexamjee2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakhloukAnassExamJee2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
